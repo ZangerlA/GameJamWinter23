9287744a18873def1168065341c8a7a4024fabd9
ZangerlA <azangerl@gmail.com>
2023-12-17 15:01:08 +0100

    asdf

--- Scripts/EndParty.gd
+++ Scripts/EndParty.gd
@@ -1,19 +1,41 @@
 extends Node2D
 
+@export var main_audio: AudioStreamPlayer2D
+@export var second_audio: AudioStreamPlayer2D
+@export var time: Timer
+
+var beat_length = 0.297  # Length of one beat in seconds
+var timer = 0.0
+var beat_counter = 0
+
 var TimeInSeconds = 15
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	pass # Replace with function body.
+	main_audio.play()
+	time.start()
+	time.timeout.connect(up_beat)
+	#time.wait_time = beat_length * 3
+	second_audio.finished.connect(continue_)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	TimeInSeconds -= delta
-	if TimeInSeconds > 0:
+	if TimeInSeconds < 0:
 		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
 
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed:
 			get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+
+func up_beat():
+	main_audio.stop()
+	second_audio.play()
+	
+	
+	
+func continue_():
+	main_audio.play()
